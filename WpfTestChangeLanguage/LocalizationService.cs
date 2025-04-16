896a56eaf74add98ba968baf97d3ec7b786753d9
Abalfazl <abalfazldev@gmail.com>
2025-04-16 13:55:35 +0330

    fix: change language

--- WpfTestChangeLanguage/LocalizationService.cs
+++ WpfTestChangeLanguage/LocalizationService.cs
@@ -5,23 +5,23 @@ using System.Linq;
 using System.Resources;
 namespace WpfTestChangeLanguage
 {
-    public class LocalizationService
+    public static class LocalizationService
     {
-        private ResourceManager _resourceManager;
-        private CultureInfo _currentCulture;
+        private static ResourceManager _resourceManager = _resourceManager = new ResourceManager("WpfTestChangeLanguage.Resources.Strings", typeof(LocalizationService).Assembly);
+        private static CultureInfo _currentCulture = CultureInfo.CurrentCulture;
 
-        public LocalizationService()
-        {
-            _resourceManager = new ResourceManager("WpfTestChangeLanguage.Resources.Strings", typeof(LocalizationService).Assembly);
-            _currentCulture = CultureInfo.CurrentCulture;
-        }
+        //public static LocalizationService()
+        //{
+        //    _resourceManager = new ResourceManager("WpfTestChangeLanguage.Resources.Strings", typeof(LocalizationService).Assembly);
+        //    _currentCulture = CultureInfo.CurrentCulture;
+        //}
 
-        public string GetString(string key)
+        public static string GetString(string key)
         {
             return _resourceManager.GetString(key, _currentCulture);
         }
 
-        public void SetCulture(string cultureCode)
+        public static void SetCulture(string cultureCode)
         {
             _currentCulture = new CultureInfo(cultureCode);
         }
--- WpfTestChangeLanguage/MainWindowViewModel.cs
+++ WpfTestChangeLanguage/MainWindowViewModel.cs
@@ -11,24 +11,27 @@ using InvestmentManage.Presentation.Helpers;
 
 namespace WpfTestChangeLanguage
 {
-    public partial class MainWindowViewModel : NotifyPropertyChanged
+    public  class MainWindowViewModel :NotifyPropertyChanged
     {
-        private readonly LocalizationService _localizationService;
+        //private readonly LocalizationService _localizationService;
+        //public LoadLanguage LoadLanguageVM {  get; set; }
 
         public MainWindowViewModel()
         {
-            _localizationService = new LocalizationService();
+            //_localizationService = new LocalizationService();
+            //LoadLanguageVM = new LoadLanguage();
             ChangeLanguage("en"); // Default Language
         }
 
-        public string MenuDashboard => _localizationService.GetString("MenuDashboard");
-        public string MenuProducts => _localizationService.GetString("MenuProducts");
+        public string MenuDashboard => LocalizationService.GetString("MenuDashboard");
+        public string MenuProducts => LocalizationService.GetString("MenuProducts");
 
         public ICommand ChangeLanguageCommand => new RelayCommand<string>(ChangeLanguage);
 
         private void ChangeLanguage(string languageCode)
         {
-            _localizationService.SetCulture(languageCode);
+            //LoadLanguageVM.ChangeLanguage(languageCode);
+            LocalizationService.SetCulture(languageCode);
             OnPropertyChanged(nameof(MenuDashboard));
             OnPropertyChanged(nameof(MenuProducts));
         }
